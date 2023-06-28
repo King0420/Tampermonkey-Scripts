cab641ed9d47e858f93ccfb4e5d1f8bde42c80ff
King0420 <104044278+King0420@users.noreply.github.com>
2023-06-28 19:14:05 +0800

    Add files via upload

---
+++ 解除网页文字无法选中和复制.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         解除网页文字无法选中
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  解除网页中文字无法选中
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAABpElEQVR4nO3Vv2uUQRDG8c/ebSMWqay0trATAxrUSi1S2AiWFoJYpNCgoBjURsHWJKeNRfAvsDgFixQqKdPZ2ViEiCJYBOQu8f1hEXO59713j7MUfLZ6d2a/O8vMO0OzDnin9Ku2Mjvuaw07xgSAYEVXe2indMhj92zpKJLnBhF8MDeye9hn6zbN70eRiqCw02Bra3up8BBLu1FEBxsBucXqW4csz0ULe4jorSCMuPU89boRELDMHiI6Y8V65bbCUTccc70RkaOwKLOg0IkyXa9qTjOu2LAs6NZuD86hrdTyxRNTkUqqdhXlHrngGRVEZsMpJwex9DxIZSHYclesIb65LCoHgIs66UJq6btDBZHZrPh8V6YBOX66LbOkTGckBYimBW2FVTNeuOZNyrFJ236Yl4NSy5SbVm1PDvhodqgyMledTdRlAtDzqfL9tfkwUtyaRkv9LwFj9B/w7wPycXOhqlJ0yZHKPChMi5MCiM47XhsopbVJAUHfrYbmN/EToN+02eLPfz9OYyZhFJzW1Jn3lTsxaKQjCkp52jy45r1ZvSbTb9M0d4PBozGZAAAAAElFTkSuQmCC
+// @author       George
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var eles = document.getElementsByTagName('*');
+    for (var i = 0; i < eles.length; i++) {
+        eles[i].style.userSelect = 'text';
+    }
+    function R(a) {
+        var ona = "on" + a;
+        if (window.addEventListener) {
+            window.addEventListener(
+                a,
+                function (e) {
+                    for (var n = e.originalTarget; n; n = n.parentNode) {
+                        n[ona] = null;
+                    }
+                },
+                true
+            );
+        }
+        window[ona] = null;
+        document[ona] = null;
+        if (document.body) {
+            document.body[ona] = null;
+        }
+    }
+
+    R("contextmenu");
+    R("click");
+    R("mousedown");
+    R("mouseup");
+    R("selectstart");
+    // Your code here...
+})();
